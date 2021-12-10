80208675f8840d7a079689604fdbf116a62d56a8
ferifahrul7 <ferifahrul7@gmail.com>
2021-12-10 16:21:07 +0700

    100 point

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,17 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          <Link href="/about">About</Link>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+        </p>
+        <p>
+          <Link href="/about">About</Link>
         </p>
 
         <div className="grid">
