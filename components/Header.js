fffd0f4e8bfcae824cee883443956a06efa3dd6a
ferifahrul7 <ferifahrul7@gmail.com>
2021-12-14 19:57:15 +0700

    fix class

--- components/Header.js
+++ components/Header.js
@@ -3,21 +3,21 @@ const Header = () => {
 	return (
 		<>
 			<header>
-				<nav class="container flex items-center justify-between py-4 mt-5 sm:mt-12">
-					<div class="py-1">
+				<nav className="container flex items-center justify-between py-4 mt-5 sm:mt-12">
+					<div className="py-1">
 						<h1 className="text-2xl font-bold text-sky-700">My Profile</h1>
 					</div>
-					<ul class="hidden sm:flex flex-1 justify-end items-center gap-12 text-blue-900 uppercase text-xs">
-						<li class="cursor-pointer nav-link">
+					<ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-blue-900 uppercase text-xs">
+						<li className="cursor-pointer nav-link">
 							<Link href="/">Home</Link>
-						</li><li class="cursor-pointer nav-link">
+						</li><li className="cursor-pointer nav-link">
 							<Link href="/about">Tentang</Link>
 						</li>
-						<li class="cursor-pointer nav-link">Kontak</li>
-						<li class="cursor-pointer nav-link">Portfolio</li>
-						<button type="button" class="bg-indigo-500 hover:bg-indigo-800 px-7 py-3 rounded-md text-white uppercase">Call</button>
+						<li className="cursor-pointer nav-link">Kontak</li>
+						<li className="cursor-pointer nav-link">Portfolio</li>
+						<button type="button" className="bg-indigo-500 hover:bg-indigo-800 px-7 py-3 rounded-md text-white uppercase">Call</button>
 					</ul>
-					<div class="flex sm:hidden flex-1 mx-3 justify-end"><i class="text-2xl fas fa-bars"></i></div>
+					<div className="flex sm:hidden flex-1 mx-3 justify-end"><i className="text-2xl fas fa-bars"></i></div>
 				</nav>
 			</header>
 		</>
